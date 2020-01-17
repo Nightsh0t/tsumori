ff880d6bd753cd4ef1321b555e14bbf264ab958e
Nightsh0t <32829880+Nightsh0t@users.noreply.github.com>
2020-01-17 01:32:22 -0500

    update api url

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-const baseUrl = 'http://localhost:8000';
+const baseUrl = 'https://tsumori.kokoro.pw/api';
 
 @Injectable({
   providedIn: 'root'
