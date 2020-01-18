bfa4ad53ab8f5c7817e98b1fd97048bc6f751551
Nightsh0t <32829880+Nightsh0t@users.noreply.github.com>
2020-01-17 19:02:00 -0500

    Fix button display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private title: Title) { }
+  constructor(private title: Title, public auth: AuthService) { }
 
   ngOnInit() {
     this.title.setTitle("Tsumori » Home");
